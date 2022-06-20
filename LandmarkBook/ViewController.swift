fc4228b4500cd4070bdbc32fbfea12e0bf04af8f
kadirakyol <kadir.akyol.ce@gmail.com>
2022-06-20 17:06:23 +0300

    First Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Kadir Akyol on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // test
+    }
+
+
+}
+
