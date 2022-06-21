f381af3bb4e99eda6c17fe19676d51ebf30dcec8
kadirakyol <kadir.akyol.ce@gmail.com>
2022-06-21 09:17:57 +0300

    added segues

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -34,8 +34,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         landmarkImages.append(UIImage(named: "Stonehenge")!)
         landmarkImages.append(UIImage(named: "TajMahal")!)
         
-        
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
@@ -45,6 +53,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return landmarkNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toImageViewController", sender: nil)
     }
 
 
---
+++ LandmarkBook/imageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  imageViewController.swift
+//  LandmarkBook
+//
+//  Created by Kadir Akyol on 21.06.2022.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var landmarkLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
